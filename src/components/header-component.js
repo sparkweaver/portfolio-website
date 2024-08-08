07a42a5e5fdc9caccb421aeadba848171e63a59a
sparkweaver <lundmark.johan.91@gmail.com>
2024-08-08 16:27:12 +0200

    feat(header): Updated the styling of the header.

--- src/components/header-component.js
+++ src/components/header-component.js
@@ -4,35 +4,55 @@ class HeaderComponent extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
     <style>
-        :host {
-            display: block;
-            background-color: #333;
-            color: #fff;
-            padding: 1rem;
-            text-align: center;
-        }
+      :host {
+        display: block;
+        background-color: #333;
+        color: #fff;
+        padding: 1rem;
+        box-sizing: border-box;
+        height: 120px;
+      }
 
-        header {
-          display: flex;
-          flex-direction: row;
-        }
+      header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        height: 100%;
+        box-sizing: border-box;
+      }
 
-        h1 {
-            margin: 0;
-            font-size: 2rem;
-        }
+      .image-container, .text-container {
+        height: 100%;
+        display: flex;
+        align-items: center;
+      }
 
-        img {
-          width:50px;
-          height:auto;
-        }
+      .text-container {
+        flex-grow: 1;
+        flex-direction: column;
+        justify-content: center;
+      }
+
+      h1, p {
+        margin: 0;
+      }
+
+      p {
+        font-style: italic;
+      }
+
+      img {
+        height: 100%;
+        width: auto;
+      }
     </style>
     <header>
-      <div>
-        <img src="src/assets/logo.png"
+      <div class="image-container">
+        <img src="src/assets/logo.png" alt="Logo"/>
       </div>
-      <div>
+      <div class="text-container text">
         <h1>${this.getAttribute('title')}</h1>
+        <p>${this.getAttribute('sub-title')}</p>
       </div>
     </header>
     `;
