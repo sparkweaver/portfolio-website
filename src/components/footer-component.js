dbf643e2ce699008e39f47220e92ee4d7337ae4c
sparkweaver <lundmark.johan.91@gmail.com>
2024-08-08 16:41:38 +0200

    feat(footer): Updated the styling of the footer.

--- src/components/footer-component.js
+++ src/components/footer-component.js
@@ -1,30 +1,39 @@
-const template = document.createElement('template');
-template.innerHTML = `
-    <style>
-        :host {
-            display: block;
-            background-color: #333;
-            width: 100%;
-            color: #fff;
-            padding: 1rem;
-            text-align: center;
-            position: fixed;
-            bottom: 0;
-            left: 0;
-        }
-    </style>
-
-    <div>
-        <p>&copy; 2024 Spark Surfers Studio. All rights reserved.</p>
-        <p>License: MIT</p>
-    </div>
-`;
-
 class FooterComponent extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+            display: block;
+            background-color: #333;
+            width: 100%;
+            height: 100px;
+            color: #fff;
+            padding: 1rem;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            box-sizing: border-box;
+        }
+
+        div {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          justify-content: center;
+        }
+
+        p {
+          margin: 0;
+        }
+      </style>
+
+      <div>
+          <p>${this.getAttribute('text')}</p>
+      </div>
+    `;
   }
 }
 
